7bf4bc573e4d10b9c3f644cb2baf0fa671afc301
Phil Danne <phil@tomesoftware.com>
2016-05-09 14:30:12 -0400

    Changed the DB URL to use EC2

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    remoteUrl : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu',
-    localUrl: 'mongodb://localhost/meanstacktutorials'
+    remoteUrl : 'mongodb://dummy:ping404tome@ec2-54-210-244-237.compute-1.amazonaws.com:27017/dummyDB',
+    localUrl: 'mongodb://localhost/dummyDB'
 };
